5b5dd592fe88d6faef08da7d9c75dca1763b134d
Heather Miller <heather.miller114@gmail.com>
2019-01-10 21:59:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/' do
-      erb :pirates :new
+    get '/new' do
+      erb :'pirates/new'
     end
 
     post '/pirates' do
