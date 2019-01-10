4614b067af573f576a11137f7763ce4481d9ef40
Heather Miller <heather.miller114@gmail.com>
2019-01-10 21:52:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,19 @@ module FormsLab
       erb :new
     end
 
+    post '/pirate'
 
-
+post '/student' do
+  @student = Student.new(params[:student])
+ 
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
 
 
   end
