3eedf2a16836f1835cdac20d9d6dd780d5953ade
Heather Miller <heather.miller114@gmail.com>
2019-01-10 21:36:11 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,7 +2,15 @@ class Ship
   
   attr_accessor :name, :type, :booty
   
+  SHIPS = []
+  
   def initialize(args)
-    @name = args
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    
+    SHIPS << self
+    
+    
   
 end
